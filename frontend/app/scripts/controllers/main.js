c69dbd1eac81594e655c4c6dc91335d164279744
Julian Godesa <julian.godesa@googlemail.com>
2015-09-19 19:43:22 +0200

    working demo

--- frontend/app/scripts/controllers/main.js
+++ frontend/app/scripts/controllers/main.js
@@ -8,9 +8,42 @@
  * Controller of the frontendApp
  */
 angular.module('frontendApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $http) {
 
+    var URL = 'http://localhost:9000/api';
     $scope.model = {};
-    $scope.model.customerNumber = '';
-    $scope.model.providers = [{label: 'DHL Kundenservice', number: 02284333112}];
+    $scope.call = call;
+    $scope.model.providers = [];
+
+    init();
+    getProviders();
+
+    function init() {
+      $scope.model.customerNumber = '';
+      $scope.model.selectedProvider = $scope.model.providers[0] || '';
+    }
+
+    function getProviders() {
+      $http.get('providers.json').then(function(response) {
+        $scope.model.providers = response.data;
+
+        if (response.data.length > 0) {
+          $scope.model.selectedProvider = $scope.model.providers[0];
+        }
+      });
+    }
+
+    function call() {
+      var payload = {
+        customerNumber: $scope.model.customerNumber,
+        provider: $scope.model.selectedProvider.key,
+        date: new Date()
+      };
+      $http.post(URL, payload)
+      .then(function (data) {
+        init();
+      }, function (err) {
+        init();
+      });
+    }
   });
